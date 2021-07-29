e9f77cefa3756a0f4b54994147b682178fe3db21
Quentin GARLAN <quenting@solina-group.fr>
2021-07-29 16:18:31 +0200

    react amplified

---
+++ react-amplified/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+import CartContextProvider from './contexts/cartContext'
+import { HelmetProvider } from 'react-helmet-async';
+import Routes from './routes';
+
+function App() {
+  return (
+          <HelmetProvider>
+            <CartContextProvider>
+              <Routes />
+            </CartContextProvider>
+          </HelmetProvider>
+    
+  );
+}
+
+export default App;
